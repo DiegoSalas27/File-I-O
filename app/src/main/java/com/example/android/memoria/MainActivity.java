7cd8923d1752f3ac4492cef3d86f0e8dd5487d52
Diego Salas <dominicsc2hs@gmail.com>
2017-08-21 15:44:36 -0500

    third commit

--- app/src/main/java/com/example/android/memoria/MainActivity.java
+++ app/src/main/java/com/example/android/memoria/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.memoria;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,7 +17,7 @@ import static com.example.android.memoria.R.id.text;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvResultado;
-    EditText etNombre;
+    EditText etNombre, etCorreo;
 
 
     @Override
@@ -26,13 +27,38 @@ public class MainActivity extends AppCompatActivity {
 
         tvResultado = (TextView) findViewById(R.id.tvResultado);
         etNombre = (EditText) findViewById(R.id.etNombre);
+        etCorreo = (EditText) findViewById(R.id.etCorreo);
+    }
+    public void generarPreferencia(View view){
+
+        //MODE_APPEND concatena los datos
+        SharedPreferences mySharedPref = getSharedPreferences("archivo_pref", Context.MODE_PRIVATE); //sobreescribe lo guardado anteriormente
+        SharedPreferences.Editor editor = mySharedPref.edit(); //inicializamos editor con la preferencia asignada
+
+        editor.putString("nombre", etNombre.getText().toString()); //guarda elementos en formato xml
+        editor.putString("correo", etCorreo.getText().toString());
+
+        editor.commit();
+
+        Toast.makeText(this, "Se ha creado la preferencia compartida!", Toast.LENGTH_SHORT).show();
+
+        etNombre.setText("");
+        etCorreo.setText("");
+    }
+    public void leerPreferencia(View view){
+
+        SharedPreferences mySharedPref = getSharedPreferences("archivo_pref", Context.MODE_PRIVATE);
+        String nombre = mySharedPref.getString("nombre", ""); // si no encuentra una variable "nombre", devuelve vacio
+        String correo = mySharedPref.getString("correo", "no existe esa variable");
+
+        tvResultado.setText("\nNombre: " + nombre + "\nCorreo: " + correo);
     }
     public void generarArchivo(View view){
         //las excepciones son importantes para entradas y salidas de datos
         try{
             String nombre = etNombre.getText().toString();
             FileOutputStream outputStream = null; //declaramos el objeto
-            outputStream = openFileOutput(getString(R.string.nombre_archivo), Context.MODE_PRIVATE); //Le damos nombre al archivo y el modo en que es creado
+            outputStream = openFileOutput(getString(R.string.nombre_archivo), Context.MODE_APPEND); //Le damos nombre al archivo y el modo en que es creado
             outputStream.write(nombre.getBytes()); //escribir el contenido del archivo
             outputStream.close();
 
